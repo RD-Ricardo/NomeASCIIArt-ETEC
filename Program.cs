520c92c6afa96ae999992276b5a76e59bde489b8
RD-Ricardo <ricardodias169@gmail.com>
2021-03-20 21:27:54 -0300

    finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+using Figgle;
+
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("-----------------");
+            Console.WriteLine("--Digite seu Nome");
+            Console.WriteLine("-----------------");
+            string name = Console.ReadLine();
+            string textao = FiggleFonts.Standard.Render(name);
+            Console.WriteLine(textao);
+        }
+    }
+}
